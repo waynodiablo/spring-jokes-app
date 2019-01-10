bbad2456bf8f1b9d0dbaf9a342cc0663abc403fc
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2019-01-10 13:14:51 +0000

    Completed joke app using Java, and Spring Framework, Boot and Initialzr, and Thymeleaf

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Boot Joke App (in Java)
+
+A first program in Java to create an app with Spring Boot that will generate Chuck Norris jokes.
+
+Following the Spring Framework Guru tutorial (https://www.youtube.com/watch?v=LhQbSgKGIYQ)
+
+
+
+
--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JokeappApplication {
+public class    JokeappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JokeappApplication.class, args);
